6e464f44ffeb476701ce75d97dce1cda601e7c74
Yonnier Uni <yo.uni@udla.edu.co>
2021-04-06 23:10:35 -0500

    Proyectil
    metodo

--- Assets/Scripts/Player/ProyectilDefinitivo.cs
+++ Assets/Scripts/Player/ProyectilDefinitivo.cs
@@ -14,13 +14,21 @@ public class ProyectilDefinitivo : MonoBehaviour
 
     void Update()
     {
-        gameObject.transform.Translate(orientacion * velX * Time.deltaTime, 0f, 0f);
+       // gameObject.transform.Translate(orientacion * velX * Time.deltaTime, 0f, 0f);
+        gameObject.transform.position=new Vector3(transform.position.x+ (orientacion * 5f*Time.deltaTime),transform.position.y,transform.position.z);
+        gameObject.transform.Rotate(0f,0f,orientacion*-1f*1f);
+        
+
     }
 
     void OnTriggerEnter2D(Collider2D c)
     {
         Destroy(gameObject, 0.1f);
     }
+     void OnCollisionEnter(Collision collision){
+                 Destroy(gameObject, 0.1f);
+
+     }
 
     public void Orientacion(float a)
     {
